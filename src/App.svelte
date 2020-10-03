12132797e2e6907d6e352558973cdff8f0e05082
Dony Cisneros <dony.cisne@gmail.com>
2020-10-03 18:55:13 -0300

    Exploring svelte app

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,50 @@
 <script>
-	export let name;
+  export let fullname;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .todos-container {
+    margin: 0 auto;
+    max-width: 600px;
+  }
+
+  .form-todo {
+    margin: 1rem 0;
+    border-radius: 4px;
+    border: 1px solid #d3d3d3;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  <h1>Todo app</h1>
+  <h2>{fullname}</h2>
+  <button> Create Todo </button>
+  <div class="todos-container">
+    <div class="form-todo">
+      <p>todo 1</p>
+    </div>
+    <div class="form-todo">
+      <p>todo 2</p>
+    </div>
+  </div>
+</main>
